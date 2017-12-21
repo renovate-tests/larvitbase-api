044d0a4d00d03b09cd44148aba409ae1a81ee170
Klas Espenkrona <klas@larvit.se>
2017-12-21 11:18:31 +0100

    Made the code runnable, no edge cases included

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
-const	Router	= require('larvitrouter'),
+const	logPrefix = 'larvitbase-api ./index.js - ',
+	Router	= require('larvitrouter'),
 	semver	= require('semver'),
 	LBase	= require('larvitbase'),
 	Lfs	= require('larvitfs'),
+	log	= require('winston'),
+	url	= require('url'),
 	fs	= require('fs');
 
-function Api(options) {
+function Api(options, cb) {
 	const	that	= this;
 
 	let	controllersFullPath,
@@ -14,13 +17,17 @@ function Api(options) {
 
 	that.options	= options;
 
+	if ( ! cb) cb = function () {};
+
 	if ( ! that.options.routerOptions)	{ that.options.routerOptions	= {};	}
 	if ( ! that.options.routerOptions.controllersPath)	{ that.options.routerOptions.controllersPath	= 'controllers';	}
 	if ( ! that.options.routerOptions.basePath)	{ that.options.routerOptions.basePath	= process.cwd();	}
 	if ( ! Array.isArray(that.options.routerOptions.routes))	{ that.options.routerOptions.routes	= [];	}
 
-	if ( ! Array.isArray(that.options.middleware)) {
-		that.options.middleware	= [];
+	if ( ! that.options.lBaseOptions) that.options.lBaseOptions = {};
+
+	if ( ! Array.isArray(that.options.lBaseOptions.middleware)) {
+		that.options.lBaseOptions.middleware	= [];
 	}
 
 	// Instantiate lfs
@@ -31,7 +38,7 @@ function Api(options) {
 	that.apiVersions	= fs.readdirSync(controllersFullPath).filter(function (file) {
 		if (
 			fs.statSync(controllersFullPath + '/' + file).isDirectory()
-			&& semver.valid(file)
+			&& semver.clean(semver.valid(file)) !== null
 		) {
 			return true;
 		} else {
@@ -48,22 +55,43 @@ function Api(options) {
 	that.router	= new Router(that.options.routerOptions);
 
 	// Default to the latest version of the API
-	that.options.middleware.push(function (req, res, cb) {
+	that.options.lBaseOptions.middleware.push(function (req, res, cb) {
 		if ( ! semver.valid(req.url.split('/')[1]) && that.apiVersions.length) {
 			req.url	= '/' + that.apiVersions[that.apiVersions.length - 1] + req.url;
 		}
 		cb();
 	});
 
+	// Parse url (Vill man kanske göra det i base ist?)
+	that.options.lBaseOptions.middleware.push(function (req, res, cb) {
+		let protocol,
+			host;
+
+		if (req.connection && req.connection.encrypted) {
+			protocol	= 'https';
+		} else {
+			protocol	= 'http';
+		}
+
+		if (req.headers && req.headers.host) {
+			host	= req.headers.host;
+		} else {
+			host	= 'localhost';
+		}
+
+		req.urlParsed = url.parse(protocol + '://' + host + req.url, true);
+		cb();
+	});
+
 	// Route the request
-	that.options.middleware.push(function (req, res, cb) {
+	that.options.lBaseOptions.middleware.push(function (req, res, cb) {
 		let	readmeFile	= false;
 
 		// Check if url is matching a directory that contains a README.md
 		if (req.url === '/' && lfs.getPathSync(that.options.routerOptions.basePath + '/README.md')) {
 			readmeFile	= that.options.routerOptions.basePath + '/README.md';
-		} else if (lfs.getPath('controllers' + req.url + '/README.md')) {
-			readmeFile	= lfs.getPath('controllers' + req.url + '/README.md');
+		} else if (lfs.getPathSync('controllers' + req.url + '/README.md')) {
+			readmeFile	= lfs.getPathSync('controllers' + req.url + '/README.md');
 		}
 
 		// If a readme file is found, send that to the browser and end the request
@@ -75,14 +103,14 @@ function Api(options) {
 			});
 		}
 
-		that.router.resolve(req.url, function (err, result) {
+		that.router.resolve(req.urlParsed.pathname, function (err, result) {
 			req.routed	= result;
 			cb(err);
 		});
 	});
 
 	// Run controller
-	that.options.middleware.push(function (req, res, cb) {
+	that.options.lBaseOptions.middleware.push(function (req, res, cb) {
 		if ( ! req.routed.controllerFullPath) {
 			res.statusCode	= 404;
 			res.data	= 'URL endpoint not found';
@@ -92,9 +120,9 @@ function Api(options) {
 	});
 
 	// Output JSON to client
-	that.options.middleware.push(function (req, res, cb) {
+	that.options.lBaseOptions.middleware.push(function (req, res, cb) {
 		res.setHeader('Content-Type', 'application/json; charset=UTF-8');
-		res.end(res.data);
+		res.end(res.data); // här måste det vara en sträng. Ska vi fixa här, i controllers eller base?
 		cb();
 	});
 
